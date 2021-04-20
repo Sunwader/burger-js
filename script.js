1e14a4eac06e2e5105064a5c8b0b40a7543a1fbf
Sunwader <sunwader@gmail.com>
2021-04-20 16:39:17 +0300

    add burger menu

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ if (menuLinks.length > 0) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
+            if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
             window.scrollTo({
                 top: gotoBlockValue,
                 behavior: "smooth"
@@ -20,27 +26,15 @@ if (menuLinks.length > 0) {
     }
 }
 
-// Мобайл & Десктоп определитель
-//  const isMobile = {
-//     Android: function () {
-//         return navigator.userAgent.match(/Android/i);
-//     },
-//     iOS: function () {
-//         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-//     },
-//     Windows: function () {
-//         return navigator.userAgent.match(/IEMObile/i);
-//     },
-//     any: function () {
-//         return(
-//             isMobile.Android() ||
-//             isMobile.iOS() ||
-//             isMobile.Windows());
-//     }
-//  };
-//
-// if (isMobile.any()) {
-//     document.body.classList.add('_touch');
-// } else {
-//     document.body.classList.add('_pc');
-// }
+//Бургер меню
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+
